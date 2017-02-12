838a70665c031cb78b77cee5a21085cb929c9b61
NIKIT <nikita2008-101@yandex.ru>
2017-02-12 18:56:23 +0300

    Add IDZ_1

---
+++ IDZ.R
@@ -0,0 +1,129 @@
+# @project IDZ_1
+# @author Nikita Fiodorov 
+# @date 12.02.2017
+# @link https://www.dropbox.com/sh/lnrmixhxjlvq72g/AABfBdX0y8r2f_BOAn3ffcI-a?dl=0
+# @data annual-diameter-of-skirt-at-hem-.csv
+
+#сначала создай main(); выборка дает случайность 2х видов
+
+#Main
+main <- function(){
+#init
+AnnualDiameter<-read.csv("IDZ_1/annual-diameter-of-skirt-at-hem-.csv");
+
+#a.	сгенерировать выборку длины 1000 из данного распределения (стр. 19)
+n <- 1000;
+rG <- rgamma(n = n, shape = 10, rate = 5.3);
+rN <- rnorm(n = n, mean = 2.3, sd = 0.3);
+rNB <- rnbinom(n = n, size = 32,prob = 1/5);
+
+#   значения функции распределения в точке x
+funP <- list(G = {function(x){ pgamma(q = x, shape = 10, rate = 5.3);}},
+             N = {function(x){ pnorm(q = x, mean = 2.3, sd = 0.3);}},
+             NB = {function(x){ pnbinom(q = x, size = 32,prob = 1/5);}}
+);
+#b.	построить по данной выборке эмпирическую функцию распределения;
+empiricalF<-function(x,t){z<-x[x<t]; length(z)/length(x)}
+empiricalFs <- c(G = empiricalF(x = 2.4,t = rG),
+                 N = empiricalF(x = 2.4,t = rN),
+                 NB = empiricalF(x = 2.4,t = rNB))
+empiricalFs
+
+#c.	построить гистограмму частот;
+
+#   Смотреть после пункта d.
+
+#d.	сравнить гистограмму частот и реальную плотность данного распределения 
+#  (вычисление значения плотности в точке 19 стр.)
+#
+ranges <- list(G = (((range(rG)[1]*100):(range(rG)[2]*100))/100),
+               N = (((range(rN)[1]*100):(range(rN)[2]*100))/100),
+               NB = range(rNB)[1]:range(rNB)[2]
+              );
+densitys <- list(G = dgamma(x = ranges$G,
+                            shape = 10, rate = 5.3),
+                 N = dnorm(x = ranges$N, 
+                           mean = 2.3, sd = 0.3),
+                 NB = dnbinom(x = ranges$NB, 
+                              size = 32,prob = 1/5))
+densitys
+
+#   построение c&d Зеленый - ген.совок. Красный - выборка
+{
+  hist(rG, 
+       breaks = 20, 
+       freq = F, 
+       col = "lightblue",
+       xlab = "Темпиратура",
+       ylab = "Плотность вероятности",
+       main = "rG Гистограмма, с кривой плотности"
+  );
+  lines(density(rG), col = "red", lwd = 2);
+  lines(x = ranges$G, y = densitys$G, col = "green", lwd = 2);
+}
+{
+  hist(rN, 
+       breaks = 20, 
+       freq = F, 
+       col = "lightblue",
+       xlab = "Темпиратура",
+       ylab = "Плотность вероятности",
+       main = "rN Гистограмма, с кривой плотности"
+  );
+  lines(density(rN), col = "red", lwd = 2);
+  lines(x = ranges$N, y = densitys$N, col = "green", lwd = 2);
+}
+{
+  hist(rNB, 
+       breaks = 20, 
+       freq = F, 
+       col = "lightblue",
+       xlab = "Темпиратура",
+       ylab = "Плотность вероятности",
+       main = "NB Гистограмма, с кривой плотности"
+  );
+  lines(density(rNB), col = "red", lwd = 2);
+  lines(x = ranges$NB, y = densitys$NB, col = "green", lwd = 2);
+}
+#e.	вычислить следующие выборочные характеристики(стр.20-22):  
+
+#   выборочное среднее
+means <- c(G = mean(rG), N = mean(rN), NB = mean(rNB));
+means
+
+#   выборочную дисперсию
+vars <- c(G = var(rG), N = var(rN), NB = var(rNB));
+vars
+
+#   выборочную асимметрию
+asm<-function(x){
+  sum((x-mean(x))^3)/length(x)/var(x)^(3/2)
+}
+asms <- c(G = asm(rG), N = asm(rN), NB = asm(rNB));
+asms
+
+#   выборочный эксцесс
+exc<-function(x){
+  sum((x-mean(x))^4)/length(x)/var(x)^2-3;
+}
+excs <- c(G = exc(rG), N = exc(rN), NB = exc(rNB));
+excs
+
+#f.	сравнить результаты пункта 'e' с реальными характеристиками распределения 
+
+meansReal <- c(G = mean(funP$G(ranges$G)), N = mean(funP$N(ranges$N)), NB = mean(funP$NB(ranges$N)));
+varsReal <- c(G = var(funP$G(ranges$G)), N = var(funP$N(ranges$N)), NB = var(funP$NB(ranges$N)));
+asmsReal <- c(G = asm(funP$G(ranges$G)), N = asm(funP$N(ranges$N)), NB = asm(funP$NB(ranges$N)));
+excsReal <- c(G = exc(funP$G(ranges$G)), N = exc(funP$N(ranges$N)), NB = exc(funP$NB(ranges$N)));
+
+meansReal
+means
+varsReal
+vars
+asmsReal
+asms
+excsReal
+excs
+
+}#endMain
+main();
